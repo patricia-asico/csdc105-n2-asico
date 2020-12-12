5596e56b86ea4dcdaf9e3f10554b550e3c816934
patricia-asico <70055131+patricia-asico@users.noreply.github.com>
2020-12-13 01:06:03 +0800

    Update test.py

--- CSDC105/csdc105-n2-asico-midterm-exam/test.py
+++ CSDC105/csdc105-n2-asico-midterm-exam/test.py
@@ -6,17 +6,17 @@
 #						completing this exercise. I am also well aware of the
 #						policies stipulated in the Adnu student handbook.
 from emmet import Emmet, Valid
+while:
+	w =input()
+	if w == "quit" or w == "exit":	#to terminate and exit the program
+		exit()
 
-w =input()
-if w == "quit" or w == "exit":	#to terminate and exit the program
-	exit()
+	check = Valid(w)
 
-check = Valid(w)
-
-if check == True:			#prints the final output
-	E = Emmet(w)
-	print(E)
+	if check == True:			#prints the final output
+		E = Emmet(w)
+		print(E)
 	
-else:
-	print("Error: Unrecognized abbreviation\n")		#prints an error message if it returned false
+	else:
+		print("Error: Unrecognized abbreviation\n")		#prints an error message if it returned false
 	
