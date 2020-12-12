4fc92af697d377e1fb2fe619bdc099da9854e6b0
patricia-asico <70055131+patricia-asico@users.noreply.github.com>
2020-12-13 01:03:20 +0800

    Create test.py

---
+++ CSDC105/csdc105-n2-asico-midterm-exam/test.py
@@ -0,0 +1,22 @@
+#Author				:	Patricia P. Asico
+#Course and Year	:	1-BSIT
+#Filename			:	emmet.py
+#Description		: 	prints the final output
+#Honor Code			:	I have not given nor received any unauthorized help in
+#						completing this exercise. I am also well aware of the
+#						policies stipulated in the Adnu student handbook.
+from emmet import Emmet, Valid
+
+w =input()
+if w == "quit" or w == "exit":	#to terminate and exit the program
+	exit()
+
+check = Valid(w)
+
+if check == True:			#prints the final output
+	E = Emmet(w)
+	print(E)
+	
+else:
+	print("Error: Unrecognized abbreviation\n")		#prints an error message if it returned false
+	
